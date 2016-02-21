df65ae1b3e8f61e15b8572cfa155a13741fd1211
avenezia <alessandro.venezia86@gmail.com>
2016-02-21 15:28:57 +0100

    Refactoring TrieTest

--- test/TrieTest.cpp
+++ test/TrieTest.cpp
@@ -1,88 +1,94 @@
-#include "TrieTest.hpp"
-
-#include <algorithm>
 #include <stdexcept>
+#include <string>
 using namespace std;
+
+#include "gtest/gtest.h"
+
+#include "Trie.hpp"
 
 namespace trie
 {
-    const string TrieTest::kDefaultKey("key");
-    const string TrieTest::kDefaultValue("value");
+    static const string kDefaultKey("key");
+    static const string kDefaultValue("value");
 
-    void TrieTest::assertTwoDeletes(const string& iFirstKeyToBeDeleted,
+    void AssertTwoDeletes(const string& iFirstKeyToBeDeleted,
         const string& iSecondKeyToBeDeleted)
     {
-        _trie->addKey(iFirstKeyToBeDeleted, kDefaultValue);
-        _trie->addKey(iSecondKeyToBeDeleted, kDefaultValue);
+        Trie<string> trie;
+        trie.addKey(iFirstKeyToBeDeleted, kDefaultValue);
+        trie.addKey(iSecondKeyToBeDeleted, kDefaultValue);
         // Checking that the first key is deleted.
-        ASSERT_TRUE(_trie->deleteKey(iFirstKeyToBeDeleted));
-        ASSERT_FALSE(_trie->hasKey(iFirstKeyToBeDeleted));
+        ASSERT_TRUE(trie.deleteKey(iFirstKeyToBeDeleted));
+        ASSERT_FALSE(trie.hasKey(iFirstKeyToBeDeleted));
         // Checking that after the first key is deleted, the second one is still there.
-        ASSERT_TRUE(_trie->hasKey(iSecondKeyToBeDeleted));
-        ASSERT_TRUE(_trie->deleteKey(iSecondKeyToBeDeleted));
-        ASSERT_FALSE(_trie->hasKey(iSecondKeyToBeDeleted));
-        ASSERT_TRUE(_trie->isEmpty());
+        ASSERT_TRUE(trie.hasKey(iSecondKeyToBeDeleted));
+        ASSERT_TRUE(trie.deleteKey(iSecondKeyToBeDeleted));
+        ASSERT_FALSE(trie.hasKey(iSecondKeyToBeDeleted));
+        ASSERT_TRUE(trie.isEmpty());
     }
 
-    void TrieTest::SetUp()
+    TEST (TrieTest, testAddKey_AddTwiceSameKeyValue)
     {
-        _trie.reset(new Trie<string>);
+        Trie<string> trie;
+        trie.addKey(kDefaultKey, kDefaultValue);
+        ASSERT_EQ(kDefaultValue, trie.getValue(kDefaultKey));
+        trie.addKey(kDefaultKey, kDefaultValue);
+        ASSERT_EQ(kDefaultValue, trie.getValue(kDefaultKey));
     }
 
-    TEST_F (TrieTest, testAddKey_AddTwiceSameKeyValue)
-    {
-        _trie->addKey(kDefaultKey, kDefaultValue);
-        ASSERT_EQ(kDefaultValue, _trie->getValue(kDefaultKey));
-        _trie->addKey(kDefaultKey, kDefaultValue);
-        ASSERT_EQ(kDefaultValue, _trie->getValue(kDefaultKey));
-    }
-
-    TEST_F (TrieTest, testAddKey_AddTwiceSameKeyWithDifferentValue)
+    TEST (TrieTest, testAddKey_AddTwiceSameKeyWithDifferentValue)
     {
         const string kNewValue("new value");
-        _trie->addKey(kDefaultKey, kDefaultValue);
-        ASSERT_EQ(kDefaultValue, _trie->getValue(kDefaultKey));
-        _trie->addKey(kDefaultKey, kNewValue);
-        ASSERT_EQ(kNewValue, _trie->getValue(kDefaultKey));
+        Trie<string> trie;
+        trie.addKey(kDefaultKey, kDefaultValue);
+        ASSERT_EQ(kDefaultValue, trie.getValue(kDefaultKey));
+        trie.addKey(kDefaultKey, kNewValue);
+        ASSERT_EQ(kNewValue, trie.getValue(kDefaultKey));
     }
 
-    TEST_F (TrieTest, testAddKey_EmptyKey)
+    TEST (TrieTest, testAddKey_EmptyKey)
     {
-        ASSERT_THROW(_trie->addKey("", kDefaultValue), runtime_error);
+        Trie<string> trie;
+        ASSERT_THROW(trie.addKey("", kDefaultValue), runtime_error);
     }
 
-    TEST_F (TrieTest, testAddKey_ValidKey)
+    TEST (TrieTest, testAddKey_ValidKey)
     {
-        _trie->addKey(kDefaultKey, kDefaultValue);
-        ASSERT_FALSE(_trie->isEmpty());
-        ASSERT_TRUE(_trie->hasKey(kDefaultKey));
+        Trie<string> trie;
+        trie.addKey(kDefaultKey, kDefaultValue);
+        ASSERT_FALSE(trie.isEmpty());
+        ASSERT_TRUE(trie.hasKey(kDefaultKey));
     }
 
-    TEST_F (TrieTest, testDeleteKey_EmptyKey)
+    TEST (TrieTest, testDeleteKey_EmptyKey)
     {
-        ASSERT_FALSE(_trie->deleteKey(""));
+        Trie<string> trie;
+        ASSERT_FALSE(trie.deleteKey(""));
     }
 
-    TEST_F (TrieTest, testDeleteKey_KeyIsNotPresentInEmptyTrie)
+    TEST (TrieTest, testDeleteKey_KeyIsNotPresentInEmptyTrie)
     {
-        ASSERT_FALSE(_trie->deleteKey(kDefaultKey));
+        Trie<string> trie;
+        ASSERT_FALSE(trie.deleteKey(kDefaultKey));
     }
 
-    TEST_F (TrieTest, testDeleteKey_KeyIsNotPresentInNotEmptyTrie)
+    TEST (TrieTest, testDeleteKey_KeyIsNotPresentInNotEmptyTrie)
     {
-        _trie->addKey(kDefaultKey, kDefaultValue);
-        ASSERT_FALSE(_trie->deleteKey(kDefaultKey + "2"));
+        Trie<string> trie;
+        trie.addKey(kDefaultKey, kDefaultValue);
+        ASSERT_FALSE(trie.deleteKey(kDefaultKey + "2"));
     }
 
-    TEST_F (TrieTest, testDeleteKey_KeyIsPresent)
+    TEST (TrieTest, testDeleteKey_KeyIsPresent)
     {
-        _trie->addKey(kDefaultKey, kDefaultValue);
-        ASSERT_TRUE(_trie->deleteKey(kDefaultKey));
-        ASSERT_FALSE(_trie->hasKey(kDefaultKey));
-        ASSERT_TRUE(_trie->isEmpty());
+        Trie<string> trie;
+        trie.addKey(kDefaultKey, kDefaultValue);
+        ASSERT_TRUE(trie.deleteKey(kDefaultKey));
+        ASSERT_FALSE(trie.hasKey(kDefaultKey));
+        ASSERT_TRUE(trie.isEmpty());
     }
 
-    TEST_F (TrieTest, testDeleteKey_MultipleDeletesOnCommonParentPrefix)
+    TEST (TrieTest, testDeleteKey_MultipleDeletesOnCommonParentPrefix)
     {
         // Checking that the delete works correctly in case of a common parent.
         // The trie is the following         / y
@@ -90,57 +96,64 @@ namespace trie
         //                                   \ j
         // The test will first delete "key" and then "kej".
         const string kSecondKey("kej");
-        assertTwoDeletes(kDefaultKey, kSecondKey);
+        AssertTwoDeletes(kDefaultKey, kSecondKey);
     }
 
-    TEST_F (TrieTest, testDeleteKey_MultipleDeletesOnSamePrefix)
+    TEST (TrieTest, testDeleteKey_MultipleDeletesOnSamePrefix)
     {
         // Checking that the delete works correctly in case the parent is deleted before the child.
         // The trie is the following k - e - y
         // The test will first delete "ke" and then "key".
         const string kPrefixKey("ke");
-        assertTwoDeletes(kPrefixKey, kDefaultKey);
+        AssertTwoDeletes(kPrefixKey, kDefaultKey);
     }
 
-    TEST_F (TrieTest, testGetValue_KeyIsNotPresent)
+    TEST (TrieTest, testGetValue_KeyIsNotPresent)
     {
-        ASSERT_THROW(_trie->getValue(kDefaultKey), runtime_error);
+        Trie<string> trie;
+        ASSERT_THROW(trie.getValue(kDefaultKey), runtime_error);
     }
 
-    TEST_F (TrieTest, testGetValue_KeyIsPresent)
+    TEST (TrieTest, testGetValue_KeyIsPresent)
     {
-        _trie->addKey(kDefaultKey, kDefaultValue);
-        ASSERT_EQ(kDefaultValue, _trie->getValue(kDefaultKey));
+        Trie<string> trie;
+        trie.addKey(kDefaultKey, kDefaultValue);
+        ASSERT_EQ(kDefaultValue, trie.getValue(kDefaultKey));
     }
 
-    TEST_F (TrieTest, testIsEmpty_EmptyTrie)
+    TEST (TrieTest, testIsEmpty_EmptyTrie)
     {
-        ASSERT_TRUE(_trie->isEmpty());
+        Trie<string> trie;
+        ASSERT_TRUE(trie.isEmpty());
     }
 
-    TEST_F (TrieTest, testIsEmpty_NotEmptyTrie)
+    TEST (TrieTest, testIsEmpty_NotEmptyTrie)
     {
-        _trie->addKey(kDefaultKey, kDefaultValue);
-        ASSERT_FALSE(_trie->isEmpty());
+        Trie<string> trie;
+        trie.addKey(kDefaultKey, kDefaultValue);
+        ASSERT_FALSE(trie.isEmpty());
     }
 
-    TEST_F (TrieTest, testHasKey_KeyIsPresent)
+    TEST (TrieTest, testHasKey_KeyIsPresent)
     {
-        _trie->addKey(kDefaultKey, kDefaultValue);
-        ASSERT_TRUE(_trie->hasKey(kDefaultKey));
+        Trie<string> trie;
+        trie.addKey(kDefaultKey, kDefaultValue);
+        ASSERT_TRUE(trie.hasKey(kDefaultKey));
     }
 
-    TEST_F (TrieTest, testHasKey_KeyIsSubstringOfOneWithValue)
+    TEST (TrieTest, testHasKey_KeyIsSubstringOfOneWithValue)
     {
+        Trie<string> trie;
         // The trie is the following: k - e - y, with only "key".
         // We check that "ke" is not found.
-        _trie->addKey(kDefaultKey, kDefaultValue);
-        ASSERT_FALSE(_trie->hasKey(kDefaultKey.substr(0, 2)));
+        trie.addKey(kDefaultKey, kDefaultValue);
+        ASSERT_FALSE(trie.hasKey(kDefaultKey.substr(0, 2)));
     }
 
-    TEST_F (TrieTest, testHasKey_KeyIsNotPresent)
+    TEST (TrieTest, testHasKey_KeyIsNotPresent)
     {
-        ASSERT_FALSE(_trie->hasKey("KeyNotPresent"));
+        Trie<string> trie;
+        ASSERT_FALSE(trie.hasKey("KeyNotPresent"));
     }
 
 }
